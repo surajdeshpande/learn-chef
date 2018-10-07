4eef8f0d8ece670b27e5b19faccd2065f45fd91e
Deshpande, Suraj <Suraj.Deshpande@cerner.com>
2018-10-07 13:47:45 +0530

    Initial Commit
    Adding Jenkins cookbook

---
+++ cookbooks/jenkins/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook:: jenkins
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+# wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+remote_file '/tmp/jenkins-ci.org.key' do
+  source 'https://pkg.jenkins.io/debian/jenkins-ci.org.key'
+  notifies :run, 'execute[apt-key add /tmp/jenkins-ci.org.key]', :immediately
+end
+
+# sudo apt-key add -
+execute 'apt-key add /tmp/jenkins-ci.org.key' do
+  action :nothing
+end
+
+# sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+file '/etc/apt/sources.list.d/jenkins.list' do
+  content 'deb http://pkg.jenkins.io/debian-stable binary/'
+  notifies :run, 'execute[apt-get update]', :immediately
+end
+
+# sudo apt-get update
+execute 'apt-get update' do
+  action :nothing
+end
+
+# Issue - https://stackoverflow.com/questions/45591869/cant-upgrade-jenkins-for-java-version-error
+package 'openjdk-7-jre-headless' do
+  action :remove
+end
+
+package 'jenkins' do
+  action :remove
+end
+
+# Dependency
+package 'openjdk-8-jre-headless'
+
+# sudo apt-get install jenkins
+package 'jenkins'
---
+++ cookbooks/jenkins/test/integration/default/default_test.rb
@@ -0,0 +1,10 @@
+# # encoding: utf-8
+
+# Inspec test for recipe jenkins::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+describe command('wget -O - http://localhost:8080') do
+  its(:stdout) { should match(/Jenkins/) }
+end
